402d7fc3e54a63105e1330ad2cfb693ae8baacb4
DuplicateP <modassirjawaid44@gmail.com>
2026-03-18 21:42:27 +0530

    Update API_ID and OWNER values in config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = os.environ.get("API_ID", "26468828")
+API_ID = os.environ.get("API_ID", "26792945")
 
 API_HASH = os.environ.get("API_HASH", "4693513c08d1ac6af15f95b116c29478")
 
@@ -8,7 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 7827463899))
+#OWNER = int(os.environ.get("OWNER", 7357284659))
 
 #LOG = -1002159628443,
 
@@ -17,8 +17,8 @@ PASS_DB = int(os.environ.get("PASS_DB", "721"))
 # auth_chats = []
 
 #try:
-    #ADMINS=[7827463899]
-    #for x in (os.environ.get("ADMINS", "7827463899").split()):
+    #ADMINS=[7357284659]
+    #for x in (os.environ.get("ADMINS", "7357284659").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
